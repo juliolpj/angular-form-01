f7c06b105e7a011f11158113450886d8c7040782
Julio Perez <juliolpj@hotmail.com>
2018-07-04 15:53:48 -0400

    0.2 Crear HTML del formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  systemTitle = 'Template Driven Form 0.1';
-  systemDescrip = 'Instalar bootstrap, font-awesome, crear navbar';
+  systemTitle = 'Template Driven Form 0.2';
+  systemDescrip = 'Crear HTML del formulario';
+  temas = ['Angular', 'React', 'VueJs'];
 }
